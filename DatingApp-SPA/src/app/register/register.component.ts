cc4cd3746ee296f30efdb9faf2bf0bf3551d0916
usaone <uddipmitra@gmail.com>
2019-10-26 20:24:13 -0400

    Sec 4 Add Register Component + Parent-Child Comm

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-register',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+    @Input() valuesFromHome: any;
+    @Output() cancelRegister = new EventEmitter();
     model: any = {};
 
     constructor() { }
@@ -18,6 +20,7 @@ export class RegisterComponent implements OnInit {
     }
 
     cancel() {
+        this.cancelRegister.emit(false);
         console.log('canceled');
     }
 }
