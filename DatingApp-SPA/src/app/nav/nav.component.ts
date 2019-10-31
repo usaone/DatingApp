d7582f382cd06a7edc0dfdf7fbb7b40a87172a29
usaone <uddipmitra@gmail.com>
2019-10-30 22:43:12 -0400

    Using Angular JWT

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -10,7 +10,7 @@ import { AlertifyService } from '../_services/alertify.service';
 export class NavComponent implements OnInit {
     model: any = {};
     
-    constructor(private authService: AuthService, private alertify: AlertifyService) { }
+    constructor(public authService: AuthService, private alertify: AlertifyService) { }
 
     ngOnInit() {
     }
@@ -24,8 +24,7 @@ export class NavComponent implements OnInit {
     }
 
     loggedIn() {
-        const token = localStorage.getItem('token');
-        return !!token; // Short hand to return false if token is null or true if token exists
+        return this.authService.loggedIn();
     }
 
     logout() {
