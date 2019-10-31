80b61e837b914eda56e259507c9bfc096b71a6b8
usaone <uddipmitra@gmail.com>
2019-10-30 21:41:44 -0400

    Steps to add alertifyjs

---
+++ DatingApp-SPA/src/app/_services/alertify.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import * as alertify from 'alertifyjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertifyService {
+
+constructor() { }
+
+}
