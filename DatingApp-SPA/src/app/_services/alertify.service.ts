49f0edc5dcd5a63d18215b4e56066adc7c72d559
usaone <uddipmitra@gmail.com>
2019-10-30 21:53:26 -0400

    See Alertify at work

--- DatingApp-SPA/src/app/_services/alertify.service.ts
+++ DatingApp-SPA/src/app/_services/alertify.service.ts
@@ -6,6 +6,30 @@ import * as alertify from 'alertifyjs';
 })
 export class AlertifyService {
 
-constructor() { }
+    constructor() { }
+
+    confirm(message: string, okCallback: () => any) {
+        alertify.confirm(message, (e: any) => {
+            if (e) {
+                okCallback();
+            } else {}
+        });
+    }
+
+    success(message: string) {
+        alertify.success(message);
+    }
+
+    error(message: string) {
+        alertify.error(message);
+    }
+
+    warning(message: string) {
+        alertify.warning(message);
+    }
+
+    message(message: string) {
+        alertify.message(message);
+    }
 
 }
